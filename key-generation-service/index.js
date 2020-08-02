af9c669de278cd633587057254e953d67722f19a
Swaminathan Muthuveerappan <swaminathanmuthuveerappan@Swaminathans-MacBook-Pro.local>
2020-08-02 21:59:49 +0530

    WIP: Docker compose

--- key-generation-service/index.js
+++ key-generation-service/index.js
@@ -6,11 +6,13 @@ const express = require('express');
 const MAX_KEY_LENGTH = 6;
 const USED_BUCKET_KEY = "keys:used";
 const NEW_BUCKET_KEY = "keys:new";
-const SERVER_PORT = "8000";
-const MAX_KEY_CACHED_COUNT = 1000000;
+const SERVER_PORT = process.env.PORT || "8000";
+const MAX_PERSISTED_NEW_KEYS_COUNT = 1000000;
 
-const inMemoryKeys = [];
-const redisClient = asyncRedis.decorate(redis.createClient());
+const redisConnectionOptions = {
+  host: process.env.REDIS_HOST || 'localhost'
+}
+const redisClient = asyncRedis.decorate(redis.createClient(redisConnectionOptions));
 const server = express();
 
 const generateRandomKey = () => nanoid(MAX_KEY_LENGTH);
@@ -33,29 +35,28 @@ const putKey = async (key) => {
   await multi.exec();
 };
 
+const getBucketSize = async (bucketKey) => {
+  return await redisClient.scard(bucketKey);
+}
+
 setInterval(async () => {
   try {
-    await putKey(generateRandomKey());
+    const count = await getBucketSize(NEW_BUCKET_KEY);
+    if (count < MAX_PERSISTED_NEW_KEYS_COUNT) {
+      await putKey(generateRandomKey());
+    }
   } catch (err) {
     console.log("error setInterval putKey " + err);
   }
 }, 100);
 
-setInterval(async () => {
-  try {
-    if (inMemoryKeys.length < MAX_KEY_CACHED_COUNT) {
-      const key = await getKey();
-      if (key) {
-        inMemoryKeys.push(key);
-      }
-    }
-  } catch (err) {
-    console.log("error setInterval getKey " + err);
-  }
-}, 100);
+server.get('/key', async (req, res) => {
+  const key = await getKey();
+  res.json({ key })
+});
 
-server.get('/', (req, res) => {
-  res.json({ key: inMemoryKeys.pop() })
+server.get('/', async (req, res) => {
+  res.sendStatus(200);
 });
 
 server.listen(SERVER_PORT, () => console.log(`Key Generation Service listening at port ${SERVER_PORT}`));
